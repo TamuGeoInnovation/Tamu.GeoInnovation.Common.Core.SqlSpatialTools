bfd4d1f1cbd82263e35f203a8718456e2c74f7c5
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-12 16:20:17 -0500

    Initial Commit

---
+++ Src/Main/KML/SqlSpatialToolKMLDocument.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SqlServer.Types;
+using SQLSpatialTools;
+
+namespace USC.GISResearchLab.Common.Core.KML
+{
+
+
+    public class SqlSpatialToolKMLDocument: KMLDocument
+    {
+        
+
+        public SqlSpatialToolKMLDocument(): base()
+
+        {
+        }
+
+        public SqlSpatialToolKMLDocument(string name): base(name)
+        {
+           
+
+        }
+
+
+
+        public void AddSqlGeometry(SqlGeometry sqlGeometry, string name, string styleName, string description)
+        {
+
+            SqlGeography sqlGeography = SQLSpatialToolsFunctions.VacuousGeometryToGeography(sqlGeometry, sqlGeometry.STSrid.Value);
+            AddSqlGeography(sqlGeography, name, styleName, description);
+        }
+
+    }
+}
